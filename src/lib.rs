71a6c92bcff11650e552e63573d98c6ad44b608c
yuma140902 <yuma140902@gmail.com>
2022-10-31 15:15:17 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::{
+    borrow::Borrow,
     fs, io,
     path::{Path, PathBuf},
 };
 
 use clap::{Parser, Subcommand};
+use handlebars::{handlebars_helper, Handlebars};
+use serde_json::{Map, Value as Json};
+use serde_yaml::Value as Yaml;
 use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
@@ -45,8 +49,26 @@ pub fn split_frontmatter(text: &str) -> (Option<String>, String) {
     }
 }
 
+handlebars_helper!(md2html: |markdown: String| {
+    let mut options = pulldown_cmark::Options::empty();
+    let parser = pulldown_cmark::Parser::new_ext(&markdown, options);
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+    html_output
+});
+
+fn make_data(yaml: &Yaml, markdown: String) -> Map<String, Json> {
+    let mut data = Map::new();
+
+    data.insert("markdown".to_owned(), handlebars::to_json(markdown));
+    data.insert("front".to_owned(), handlebars::to_json(yaml));
+
+    data
+}
+
 fn single_generate(
     filepath: PathBuf,
+    handlebars: &Handlebars,
     pages_directory: impl AsRef<Path>,
     output_directory: impl AsRef<Path>,
 ) -> io::Result<()> {
@@ -61,12 +83,15 @@ fn single_generate(
     );
 
     let content = fs::read_to_string(filepath)?;
-    let (_maybe_yaml, markdown) = split_frontmatter(&content);
+    let (maybe_yaml, markdown) = split_frontmatter(&content);
+    let yaml = maybe_yaml.unwrap();
+    let yaml: Yaml = serde_yaml::from_str(&yaml).unwrap();
 
-    let mut options = pulldown_cmark::Options::empty();
-    let parser = pulldown_cmark::Parser::new_ext(&markdown, options);
-    let mut html_output = String::new();
-    pulldown_cmark::html::push_html(&mut html_output, parser);
+    let data = make_data(&yaml, markdown);
+
+    let html_output = handlebars
+        .render(yaml["template"].as_str().unwrap(), &data)
+        .unwrap();
 
     if let Some(parent) = output_filepath.parent() {
         fs::create_dir_all(parent)?;
@@ -80,9 +105,39 @@ fn get_output_relative_path(file: impl AsRef<Path>, pages_directory: impl AsRef<
     pathdiff::diff_paths(file, pages_directory).unwrap()
 }
 
+fn get_template_relative_path(
+    file: impl AsRef<Path>,
+    templates_directory: impl AsRef<Path>,
+) -> PathBuf {
+    pathdiff::diff_paths(file, templates_directory).unwrap()
+}
+
+fn setup_handlebars(templates_directory: impl AsRef<Path>) -> Handlebars<'static> {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_helper("md2html", Box::new(md2html));
+
+    for path in WalkDir::new(&templates_directory)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .map(|e| e.path().to_path_buf())
+        .filter(|p| p.is_file() && p.extension().map(|ext| ext == "hbs").unwrap_or(false))
+    {
+        let relative_path = get_template_relative_path(&path, &templates_directory);
+        handlebars
+            .register_template_file(relative_path.to_string_lossy().borrow(), path)
+            .unwrap();
+        println!("registered template: {}", relative_path.display());
+    }
+
+    handlebars
+}
+
 pub fn generate(root_directory: impl AsRef<Path>) -> io::Result<()> {
     let pages_directory = get_pages_directory(&root_directory);
+    let templates_directory = get_templates_directory(&root_directory);
     let output_directory = get_output_directory(&root_directory);
+
+    let handlebars = setup_handlebars(templates_directory);
 
     for filepath in WalkDir::new(&pages_directory)
         .into_iter()
@@ -90,7 +145,7 @@ pub fn generate(root_directory: impl AsRef<Path>) -> io::Result<()> {
         .map(|e| e.path().to_path_buf())
         .filter(|p| p.is_file() && p.extension().map(|ext| ext == "md").unwrap_or(false))
     {
-        let result = single_generate(filepath, &pages_directory, &output_directory);
+        let result = single_generate(filepath, &handlebars, &pages_directory, &output_directory);
         if let Err(err) = result {
             eprintln!("error: {:?}", err);
         }
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,23 @@ fn main() -> io::Result<()> {
         Commands::Init { directory } => {
             let pages = tempura::get_pages_directory(&directory);
             fs::create_dir_all(&pages)?;
+            fs::write(
+                pages.join("sample.md"),
+                include_str!("../resources/sample.md"),
+            )?;
+            fs::create_dir_all(pages.join("sub_dir"))?;
+            fs::write(
+                pages.join("sub_dir/sample2.md"),
+                include_str!("../resources/sample2.md"),
+            )?;
             println!("setup pages directory: {}", pages.to_string_lossy());
 
             let templates = tempura::get_templates_directory(&directory);
             fs::create_dir_all(&templates)?;
+            fs::write(
+                templates.join("page.html.hbs"),
+                include_str!("../resources/page.html.hbs"),
+            )?;
             println!("setup templates directory: {}", templates.to_string_lossy());
 
             let output = tempura::get_output_directory(&directory);
