109e4c5c669aa9de0ac843eb3d9c1d6ee33ce110
yuma140902 <yuma140902@gmail.com>
2022-10-31 13:19:28 +0900

    add init subcommand

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    pub command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    Init {},
+    Init {
+        #[arg(default_value = ".")]
+        directory: PathBuf,
+    },
     Gen {},
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-use clap::Parser;
-use tempura::Cli;
+use std::{fs, io};
 
-fn main() {
+use clap::Parser;
+use tempura::{Cli, Commands};
+
+fn main() -> io::Result<()> {
     let cli = Cli::parse();
 
+    match &cli.command {
+        Commands::Init { directory } => {
+            let markdowns = directory.join("markdowns");
+            let templates = directory.join("templates");
+            fs::create_dir_all(&markdowns)?;
+            println!("setup directory: {}", markdowns.to_string_lossy());
+            fs::create_dir_all(&templates)?;
+            println!("setup directory: {}", templates.to_string_lossy());
+        }
+        Commands::Gen {} => {}
+    }
+
     println!("{:?}", cli);
+
+    Ok(())
 }
