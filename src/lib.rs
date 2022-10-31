66b3f62657dafdc433b673e6a537ffbe2d6d373b
yuma140902 <yuma140902@gmail.com>
2022-10-31 14:05:05 +0900

    handle path

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::{
+    io,
+    path::{Path, PathBuf},
+};
 
 use clap::{Parser, Subcommand};
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -15,5 +19,61 @@ pub enum Commands {
         #[arg(default_value = ".")]
         directory: PathBuf,
     },
-    Gen {},
+    Gen {
+        #[arg(default_value = ".")]
+        directory: PathBuf,
+    },
+}
+
+pub fn get_pages_directory(root_directory: impl AsRef<Path>) -> PathBuf {
+    root_directory.as_ref().join("pages")
+}
+
+pub fn get_templates_directory(root_directory: impl AsRef<Path>) -> PathBuf {
+    root_directory.as_ref().join("templates")
+}
+
+pub fn get_output_directory(root_directory: impl AsRef<Path>) -> PathBuf {
+    root_directory.as_ref().join("public")
+}
+
+fn single_generate(filepath: PathBuf, output_directory: impl AsRef<Path>) -> io::Result<()> {
+    let output_directory = output_directory.as_ref().to_path_buf();
+    let output_filepath = output_directory.join(&filepath);
+    println!(
+        "processing {} -> {}",
+        filepath.display(),
+        output_filepath.display()
+    );
+    Ok(())
+}
+
+fn get_output_relative_path(
+    file: impl AsRef<Path>,
+    _pages_directory: impl AsRef<Path>,
+) -> io::Result<PathBuf> {
+    if file.as_ref().is_relative() {
+        Ok(file.as_ref().to_path_buf())
+    } else {
+        todo!()
+    }
+}
+
+pub fn generate(root_directory: impl AsRef<Path>) -> io::Result<()> {
+    let pages_directory = get_pages_directory(&root_directory);
+    let output_directory = get_output_directory(&root_directory);
+
+    for filepath in WalkDir::new(pages_directory)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .map(|e| e.path().to_path_buf())
+        .filter(|p| p.is_file() && p.extension().map(|ext| ext == "md").unwrap_or(false))
+    {
+        let result = single_generate(filepath, &output_directory);
+        if let Err(err) = result {
+            eprintln!("error: {:?}", err);
+        }
+    }
+    println!("Done.");
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use std::{fs, io};
 
 use clap::Parser;
-use tempura::{Cli, Commands};
+use tempura::{generate, Cli, Commands};
 
 fn main() -> io::Result<()> {
     let cli = Cli::parse();
+    println!("{:?}", cli);
 
     match &cli.command {
         Commands::Init { directory } => {
-            let markdowns = directory.join("markdowns");
-            let templates = directory.join("templates");
-            fs::create_dir_all(&markdowns)?;
-            println!("setup directory: {}", markdowns.to_string_lossy());
-            fs::create_dir_all(&templates)?;
-            println!("setup directory: {}", templates.to_string_lossy());
-        }
-        Commands::Gen {} => {}
-    }
+            let pages = tempura::get_pages_directory(&directory);
+            fs::create_dir_all(&pages)?;
+            println!("setup pages directory: {}", pages.to_string_lossy());
 
-    println!("{:?}", cli);
+            let templates = tempura::get_templates_directory(&directory);
+            fs::create_dir_all(&templates)?;
+            println!("setup templates directory: {}", templates.to_string_lossy());
+
+            let output = tempura::get_output_directory(&directory);
+            fs::create_dir_all(&output)?;
+            println!("setup output directory: {}", output.to_string_lossy());
+
+            println!("setup done.");
+        }
+        Commands::Gen { directory } => generate(directory)?,
+    }
 
     Ok(())
 }
