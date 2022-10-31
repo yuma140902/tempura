5308b3598ea00eda869f47b6abd2e43daa6590c7
yuma140902 <yuma140902@gmail.com>
2022-10-31 14:24:15 +0900

    markdown to html

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    io,
+    fs, io,
     path::{Path, PathBuf},
 };
 
@@ -37,39 +37,52 @@ pub fn get_output_directory(root_directory: impl AsRef<Path>) -> PathBuf {
     root_directory.as_ref().join("public")
 }
 
-fn single_generate(filepath: PathBuf, output_directory: impl AsRef<Path>) -> io::Result<()> {
+fn single_generate(
+    filepath: PathBuf,
+    pages_directory: impl AsRef<Path>,
+    output_directory: impl AsRef<Path>,
+) -> io::Result<()> {
     let output_directory = output_directory.as_ref().to_path_buf();
-    let output_filepath = output_directory.join(&filepath);
+    let mut output_filepath =
+        output_directory.join(get_output_relative_path(&filepath, pages_directory));
+    output_filepath.set_extension("html");
     println!(
         "processing {} -> {}",
         filepath.display(),
         output_filepath.display()
     );
+
+    let content = fs::read_to_string(filepath)?;
+    let (maybe_yaml, markdown) = frontmatter::parse_and_find_content(&content).unwrap();
+
+    let mut options = pulldown_cmark::Options::empty();
+    let parser = pulldown_cmark::Parser::new_ext(markdown, options);
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+
+    if let Some(parent) = output_filepath.parent() {
+        fs::create_dir_all(parent)?;
+    }
+    fs::write(&output_filepath, html_output)?;
+
     Ok(())
 }
 
-fn get_output_relative_path(
-    file: impl AsRef<Path>,
-    _pages_directory: impl AsRef<Path>,
-) -> io::Result<PathBuf> {
-    if file.as_ref().is_relative() {
-        Ok(file.as_ref().to_path_buf())
-    } else {
-        todo!()
-    }
+fn get_output_relative_path(file: impl AsRef<Path>, pages_directory: impl AsRef<Path>) -> PathBuf {
+    pathdiff::diff_paths(file, pages_directory).unwrap()
 }
 
 pub fn generate(root_directory: impl AsRef<Path>) -> io::Result<()> {
     let pages_directory = get_pages_directory(&root_directory);
     let output_directory = get_output_directory(&root_directory);
 
-    for filepath in WalkDir::new(pages_directory)
+    for filepath in WalkDir::new(&pages_directory)
         .into_iter()
         .filter_map(|e| e.ok())
         .map(|e| e.path().to_path_buf())
         .filter(|p| p.is_file() && p.extension().map(|ext| ext == "md").unwrap_or(false))
     {
-        let result = single_generate(filepath, &output_directory);
+        let result = single_generate(filepath, &pages_directory, &output_directory);
         if let Err(err) = result {
             eprintln!("error: {:?}", err);
         }
