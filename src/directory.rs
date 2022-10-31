cbb6a975c4356c6d6746da7fa1643698c961d5b6
yuma140902 <yuma140902@gmail.com>
2022-11-01 03:33:15 +0900

    BREAKING_CHANGE: change pages and templates directory

--- src/directory.rs
+++ src/directory.rs
@@ -1,11 +1,11 @@
 use std::path::{Path, PathBuf};
 
 pub fn get_pages_directory(root_directory: impl AsRef<Path>) -> PathBuf {
-    root_directory.as_ref().join("pages")
+    root_directory.as_ref().join("src/pages")
 }
 
 pub fn get_templates_directory(root_directory: impl AsRef<Path>) -> PathBuf {
-    root_directory.as_ref().join("templates")
+    root_directory.as_ref().join("src/templates")
 }
 
 pub fn get_output_directory(root_directory: impl AsRef<Path>) -> PathBuf {
