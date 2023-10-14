6b5dc8c448299485d2b07fdb6eef4dd584f11bb6
yuma14 <yuma140902@gmail.com>
2023-10-15 00:06:23 +0900

    add TemplateEngine

---
+++ src/template_engine.rs
@@ -0,0 +1,17 @@
+use handlebars::Handlebars;
+
+use crate::handlebars_helpers;
+
+/// TemplateEngine is a wrapper for Handlebars and processes templates and [`Value`](crate::Value)s.
+pub struct TemplateEngine(Handlebars<'static>);
+
+impl TemplateEngine {
+    pub fn new() -> Self {
+        let mut handlebars = Handlebars::new();
+
+        handlebars.register_helper("md2html", Box::new(handlebars_helpers::md2html));
+        handlebars.register_helper("resolve", Box::new(handlebars_helpers::resolve));
+
+        Self(handlebars)
+    }
+}
