4a3afe122edf384c334d0d7a0a4902462a55f3f0
yuma14 <yuma140902@gmail.com>
2023-11-10 00:41:22 +0900

    initコマンドを未実装状態に戻す

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use std::{fs, io, path::Path};
 
 use anyhow::Context;
-use tracing::info;
 
 use crate::project_config::ProjectConfig;
 
@@ -39,53 +38,7 @@ pub fn build(project_root: &Path) -> anyhow::Result<()> {
 
 #[tracing::instrument]
 pub fn init(project_root: &Path) -> io::Result<()> {
-    let pages = directory::get_pages_directory(project_root);
-    fs::create_dir_all(&pages)?;
-    fs::write(
-        pages.join("sample.md"),
-        include_str!("../resources/sample.md"),
-    )?;
-    fs::write(
-        pages.join("style.css"),
-        include_str!("../resources/style.css"),
-    )?;
-    fs::create_dir_all(pages.join("sub_dir"))?;
-    fs::write(
-        pages.join("sub_dir/sample2.md"),
-        include_str!("../resources/sample2.md"),
-    )?;
-    info!("setup pages directory: {}", pages.display());
-
-    let templates = directory::get_templates_directory(project_root);
-    fs::create_dir_all(&templates)?;
-    fs::write(
-        templates.join("page.html.hbs"),
-        include_str!("../resources/page.html.hbs"),
-    )?;
-    info!("setup templates directory: {}", templates.display());
-
-    let output = directory::get_output_directory(project_root);
-    fs::create_dir_all(&output)?;
-    info!("setup output directory: {}", output.display());
-
-    let config_file = directory::get_project_config_path(project_root);
-    let config = ProjectConfig::default();
-    let config_json = serde_json::to_string_pretty(&config)?;
-    fs::write(&config_file, config_json)?;
-    info!("setup project config file: {}", config_file.display());
-
-    let gitignore = project_root.join(".gitignore");
-    fs::write(&gitignore, include_str!("../resources/gitignore"))?;
-    info!("setup .gitignore file: {}", gitignore.display());
-
-    info!("setup done.");
-
-    println!();
-    println!("Setup done. To build website, run:");
-    println!();
-    println!("  cd {}", project_root.display());
-    println!("  tempura build .");
-    println!();
+    println!("not implemented yet");
 
     Ok(())
 }
